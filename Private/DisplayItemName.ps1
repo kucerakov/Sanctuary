39e07c1020177a5109d5a286e5a68f2f7263eedc
Kucerakov <Kucerakov05@gmail.com>
2021-02-27 22:48:37 -0600

    Get recipe complete

---
+++ Private/DisplayItemName.ps1
@@ -0,0 +1,14 @@
+﻿Function DisplayItemName{
+    [CmdletBinding()]
+	param (
+		# Array of items
+		[Parameter(Mandatory = $true)]
+		[int]
+		$itemID
+	)
+    $itemID = $item.item_id
+	$itemDetails = Select-MySQL -Connection $connection -table "items" -where "id = $itemID"
+    return $itemDetails.Name
+}
+
+Export-ModuleMember -Function DisplayItemName
---
+++ Sanctuary.psm1
@@ -0,0 +1,15 @@
+﻿New-Variable -Name ModuleRoot -value $PSScriptRoot -Option ReadOnly
+$Public = @(Get-ChildItem -Path $ModuleRoot\Public -include *.ps1 -Recurse -ErrorAction SilentlyContinue)
+$Private = @(Get-ChildItem -Path $ModuleRoot\Private -include *.ps1 -Recurse -ErrorAction SilentlyContinue)
+
+foreach ($import in @($Public + $Private)){
+    try {
+        . $Import.FullName
+    } catch{
+        Write-Error -Message "Failed to import fuction $($Import.BaseName): $_"
+    }
+}
+
+foreach ($publicFunction in $Public) {
+    Export-ModuleMember -Function $publicFunction.BaseName
+}
