7c5ba338e946b4307abe936c0f06d2162533b223
Kucerakov <Kucerakov05@gmail.com>
2021-02-27 20:04:45 -0600

    bug fix

--- Public/Get-MySQLConnection.ps1
+++ Public/Get-MySQLConnection.ps1
@@ -4,6 +4,6 @@ Function Get-MySQLConnection {
 	$database = Read-Host "Enter the database name"
 	$username = Read-Host "Enter your username"
 	$password = Read-Host "Enter your password"
-	$connection - Connect-MySQL -user $username -password $password -Database $database -server $server -port $port
+	$connection = Connect-MySQL -user $username -password $password -Database $database -server $server -port $port
 	return $connection
 }
