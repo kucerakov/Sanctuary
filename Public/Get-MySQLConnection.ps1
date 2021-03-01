a9f3609ebd318832fd9b7085aac588b89747cef0
Kucerakov <Kucerakov05@gmail.com>
2021-02-28 21:45:32 -0600

    update get-mysqlconnection to be generic

--- Public/Get-MySQLConnection.ps1
+++ Public/Get-MySQLConnection.ps1
@@ -1,12 +1,12 @@
 Function Get-MySQLConnection {
-	#$server = Read-Host "Enter the server IP/Hostname"
+	$computerName = Read-Host "Enter the server IP/Hostname"
 	#$port = Read-Host "Enter what port to connect to"
 	#$database = Read-Host "Enter the database name"
 	#$username = Read-Host "Enter your username"
 	#$password = Read-Host "Enter your password"
 	#$connection = Connect-MySQL -user $username -password $password -Database $database -server $server -port $port
     $creds = (Get-Credential)
-	$connection = Connect-MySQLServer -Credential $creds -ComputerName 'localhost' -database peq
+	$connection = Connect-MySQLServer -Credential $creds -ComputerName $computerName -database peq
 	return $connection
 }
 
